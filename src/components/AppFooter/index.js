c0b729b41504ad05e8d605ec31cd3d6cf56d44c2
Séverine Cuenot <nine.injections@gmail.com>
2023-11-06 11:27:26 +0100

    :art: style : starting to style footer

--- src/components/AppFooter/index.js
+++ src/components/AppFooter/index.js
@@ -11,32 +11,33 @@ function AppFooter() {
     <footer className="footer">
       <div className="footer__topblock">
         <div className="footer__right-block">
-          <h1>LLLPE</h1>
-          <p>
+          <h1 className="footer__right-block-title header">LLLPE</h1>
+          <p className="footer__right-block-text">
             Un espace de débat et de projets par les habitant.e.s de la rue et de la ville
           </p>
         </div>
         <nav className="footer__left-block">
           <ul>
-            <h2>Explorer</h2>
+            <h2 className="footer__left-block-title">Explorer</h2>
             <li>Unipopia</li>
             <li>Le Lîeu</li>
             <li>Le Parlons-en</li>
           </ul>
           <ul>
-            <h2>Contenu multimédia</h2>
+            <h2 className="footer__left-block-title">Contenu multimédia</h2>
             <li>Vidéos</li>
             <li>Livrets</li>
             <li>Podcasts</li>
             <li>Photos</li>
           </ul>
           <ul>
-            <h2>à propos</h2>
+            <h2 className="footer__left-block-title">à propos</h2>
             <li>Nous trouver</li>
             <li>Nous contacter</li>
           </ul>
         </nav>
       </div>
+      <hr className="footer__separation" />
       <div className="footer__copyright">
         <p>Tous droits réservés &copy; Séverine Cuenot {year}<br />
           <Link to="/credits" className="footer__link">&#9825; Crédits </Link>

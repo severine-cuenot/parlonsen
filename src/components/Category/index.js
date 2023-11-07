8216e5141ae673c3091ec56e5a3945741ce52f97
Séverine Cuenot <nine.injections@gmail.com>
2023-11-07 09:42:45 +0100

    :rocket: feat: added cards for categories. Still need desktop media queries, button and style

--- src/components/Category/index.js
+++ src/components/Category/index.js
@@ -1,15 +1,37 @@
 /* eslint-disable max-len */
+// Components import
+
 // Imports
 import './style.scss';
+import card1 from '../../../public/img/reunion.jpg';
 
 function Category() {
   return (
-    <section>
-      <div className="hero__left-block">
-        <header className="hero__title header">LLLPE</header>
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio quae a asperiores facilis ipsa molestias quis exercitationem nesciunt obcaecati, perferendis porro atque cumque praesentium non aliquid error consequuntur voluptatibus doloribus?</p>
-      </div>
-      <div className="hero__right-block" />
+    <section className="categories">
+      <article className="oneCategory">
+        <div>
+          <img src={card1} alt="Illustration de nos réuions" className="oneCategory__img" />
+        </div>
+        <div>
+          <h2 className="oneCategory__title">Unipopia</h2>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minus, assumenda? Beatae atque quos rerum amet, iusto facere officiis veritatis nam eaque officia error! Dicta numquam illum, voluptatem voluptatibus animi repellat.</p>
+        </div>
+        <div>BTN</div>
+      </article>
+      <article className="oneCategory">
+        <div>
+          <img src={card1} alt="Illustration de nos réuions" className="oneCategory__img" />
+        </div>
+        <div>TEXTE</div>
+        <div>BTN</div>
+      </article>
+      <article className="oneCategory">
+        <div>
+          <img src={card1} alt="Illustration de nos réuions" className="oneCategory__img" />
+        </div>
+        <div>TEXTE</div>
+        <div>BTN</div>
+      </article>
     </section>
   );
 }

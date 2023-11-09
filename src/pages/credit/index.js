c091db0e55bfbc9b1d0fa5c25016e80c37367d43
Séverine Cuenot <nine.injections@gmail.com>
2023-11-09 09:58:54 +0100

    :rocket: feat: added credits page

---
+++ src/pages/credit/index.js
@@ -0,0 +1,23 @@
+/* eslint-disable max-len */
+// Component imports
+import Button from '../../components/Button';
+// imports
+import './style.scss';
+
+function Credits() {
+  return (
+    <section className="credits">
+      <header className="credits__header header">
+        <h2>Crédits</h2>
+      </header>
+      <div className="credits__iframe-block">
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aperiam aut porro doloremque voluptatibus, ea tempore libero quis nisi sunt alias voluptate aliquam! Corrupti distinctio magni molestias excepturi amet cumque aut!
+        </p>
+        <Button />
+      </div>
+    </section>
+  );
+}
+
+export default Credits;

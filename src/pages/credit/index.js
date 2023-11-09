a481017fbf3127753caa852e09defb129244af11
Séverine Cuenot <nine.injections@gmail.com>
2023-11-09 10:33:54 +0100

    :art: style : CSS done for credits. Added a color in vars

--- src/pages/credit/index.js
+++ src/pages/credit/index.js
@@ -1,22 +1,30 @@
+/* eslint-disable jsx-a11y/no-redundant-roles */
 /* eslint-disable max-len */
+// React imports
+import { Link } from 'react-router-dom';
+
 // Component imports
 import Button from '../../components/Button';
+import Container from '../../components/Container';
 // imports
 import './style.scss';
 
 function Credits() {
   return (
-    <section className="credits">
-      <header className="credits__header header">
-        <h2>Crédits</h2>
-      </header>
-      <div className="credits__iframe-block">
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aperiam aut porro doloremque voluptatibus, ea tempore libero quis nisi sunt alias voluptate aliquam! Corrupti distinctio magni molestias excepturi amet cumque aut!
-        </p>
-        <Button />
-      </div>
-    </section>
+    <Container className="container__credits">
+      <section className="credits">
+        <header className="credits__header header">
+          <h2>Crédits</h2>
+        </header>
+        <ul className="credits__text" role="list">
+          <li>&diams; Icon des vidéos créée par <a href="https://www.flaticon.com/free-icons/video" target="_blank" rel="noreferrer" title="video icons">Freepik - Flaticon</a></li>
+          <li>&diams; Icon des livres créée par <a href="https://www.flaticon.com/free-icons/book" title="book icons" target="_blank" rel="noreferrer">mikan933 - Flaticon</a></li>
+          <li>&diams; Icon des podcasts créée par<a href="https://www.flaticon.com/free-icons/podcast" title="podcast icons" target="_blank" rel="noreferrer"> Freepik - Flaticon</a></li>
+          <li>&diams; Icon des images créée par <a href="https://www.flaticon.com/free-icons/picture" title="picture icons" target="_blank" rel="noreferrer">FauzIDEA - Flaticon</a></li>
+        </ul>
+        <Link to="/"><Button type="button" label="Retourner à l'accueil" /></Link>
+      </section>
+    </Container>
   );
 }
 

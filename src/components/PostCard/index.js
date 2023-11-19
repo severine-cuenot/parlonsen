775355b7f93eee26549c2e5d838abc29bc90a265
Séverine Cuenot <nine.injections@gmail.com>
2023-11-19 16:08:19 +0100

    last try and still doesn't work

--- src/components/PostCard/index.js
+++ src/components/PostCard/index.js
@@ -1,12 +1,20 @@
+/* eslint-disable max-len */
+/* eslint-disable react/no-array-index-key */
+
+// React imports
 import { useState } from 'react';
 import PropTypes from 'prop-types';
 import { RichText } from '@graphcms/rich-text-react-renderer';
+// import Lightbox from 'react-awesome-lightbox';
+import 'react-awesome-lightbox/build/style.css';
 import ModalImage from 'react-modal-image';
+
+// imports
 import './style.scss';
 
 function PostCard({ posts }) {
   const unipopiaPosts = posts.filter((post) => post.node.categories.some((category) => category.nom === 'Unipopia'));
-  const [clickedImage, setClickedImage] = useState(null);
+  const [clickedImage, setClickedImage] = useState('');
 
   return (
     <article className="post__block">
@@ -19,29 +27,33 @@ function PostCard({ posts }) {
             {unipopia.node.extrait}
           </div>
           <div className="post__content">
+            {console.log('Contenu brut :', unipopia.node.contenu.raw)}
             <RichText
               content={unipopia.node.contenu.raw}
               renderers={{
                 image: ({ node }) => (
-                  <div>
-                    <ModalImage
+                  <a href={`https://media.graphassets.com/resize=width:${node.width},height:${node.height}/${node.handle}`} target="_blank" rel="noreferrer">
+                    <img
                       alt={node.title}
-                      small={node.src}
-                      large={node.src}
-                      hideZoom
-                      onClick={() => setClickedImage(node.src)}
+                      src={node.src}
+                      height={node.height}
+                      width={node.width}
+                      onClick={() => {
+                        setClickedImage(node.src ?? '');
+                      }}
                     />
-                  </div>
+                  </a>
                 ),
               }}
             />
             {clickedImage && (
-              <ModalImage
-                alt="Clicked Image"
-                small={clickedImage}
-                large={clickedImage}
-                onClose={() => setClickedImage(null)}
-              />
+            <ModalImage
+              large={clickedImage}
+              title="Image Title"
+          // @ts-expect-error The types for this library aren't updated, but it does support the onClose prop
+          // See https://github.com/aautio/react-modal-image/tree/master#lightbox-only-api-for-advanced-usage
+              onClose={() => setClickedImage(null)}
+            />
             )}
           </div>
         </div>

47d17224c24edbdb74a186d9880ab6dbd528e916
Séverine Cuenot <nine.injections@gmail.com>
2023-11-21 15:41:45 +0100

    :rocket: feat: finished player. Need css.

--- src/components/OnePodcast/index.js
+++ src/components/OnePodcast/index.js
@@ -1,8 +1,9 @@
+/* eslint-disable jsx-a11y/media-has-caption */
 // React imports
 import { IoPlaySharp } from 'react-icons/io5';
 import { AiFillBackward, AiFillForward } from 'react-icons/ai';
 import { IoMdPause } from 'react-icons/io';
-import { useState, useRef } from 'react';
+import { useState, useRef, useEffect } from 'react';
 
 // Component imports
 import Container from '../Container';
@@ -12,9 +13,65 @@ import './style.scss';
 import TestAudio from '../../../public/pe_2022_03_17_treve_hivernale.mp3';
 
 function OnePodcast() {
+  // State
   const [isPlaying, setIsPlaying] = useState(false);
+  const [currentTime, setCurrentTime] = useState(0);
+  const [duration, setDuration] = useState(0);
+
+  // References
+  const audioPlayer = useRef(); // reference the audioplayer component
+  const progressBar = useRef(); // reference the progress bar
+  const animationRef = useRef(); // reference the animation
+
+  const onLoadedMetadata = () => {
+    setDuration(Math.floor(audioPlayer.current.duration));
+  };
+
+  const calculateTime = (secs) => {
+    const minutes = Math.floor(secs / 60);
+    const returnedMinutes = minutes < 10 ? `0${minutes}` : `${minutes}`;
+    const seconds = Math.floor(secs % 60);
+    const returnedSeconds = seconds < 10 ? `0${seconds}` : `${seconds}`;
+    return `${returnedMinutes}:${returnedSeconds} `;
+  };
+
   const togglePlayPause = () => {
-    setIsPlaying(!isPlaying);
+    const prevValue = isPlaying;
+    setIsPlaying(!prevValue);
+    if (!prevValue) {
+      audioPlayer.current.play();
+      animationRef.current = requestAnimationFrame(whilePlaying);
+    }
+    else {
+      audioPlayer.current.pause();
+      cancelAnimationFrame(animationRef.current);
+    }
+  };
+
+  const whilePlaying = () => {
+    progressBar.current.value = audioPlayer.current.currentTime;
+    changePlayerCurrentTime();
+    animationRef.current = requestAnimationFrame(whilePlaying);
+  };
+
+  const changeRange = () => {
+    audioPlayer.current.currentTime = progressBar.current.value;
+    changePlayerCurrentTime();
+  };
+
+  const changePlayerCurrentTime = () => {
+    progressBar.current.style.setProperty('--seek-before-width', `${progressBar.current.value / duration * 100}%`);
+    setCurrentTime(progressBar.current.value);
+  };
+
+  const backThirty = () => {
+    progressBar.current.value = Number(progressBar.current.value - 30);
+    changeRange();
+  };
+
+  const forwardThirty = () => {
+    progressBar.current.value = Number(progressBar.current.value + 30);
+    changeRange();
   };
 
   return (
@@ -25,23 +82,23 @@ function OnePodcast() {
         </header>
         <div className="audioPlayer">
 
-          <audio src={TestAudio} preload="metadata" />
-          <button type="button"><AiFillBackward /> 30 </button>
+          <audio ref={audioPlayer} src={TestAudio} onLoadedMetadata={onLoadedMetadata} preload="metadata" />
+          <button type="button" onClick={backThirty}><AiFillBackward /> 30 </button>
           <button type="button" onClick={togglePlayPause}>
             {isPlaying ? <IoMdPause /> : <IoPlaySharp /> }
           </button>
-          <button type="button"> 30 <AiFillForward /></button>
+          <button type="button" onClick={forwardThirty}> 30 <AiFillForward /></button>
 
           {/* current time */}
-          <div>0:00</div>
+          <div>{calculateTime(currentTime)}</div>
 
           {/* Progress bar */}
           <div>
-            <input type="range" className="audioPlayer__progressBar" />
+            <input type="range" className="audioPlayer__progressBar" defaultValue="0" ref={progressBar} onChange={changeRange} />
           </div>
 
           {/* duration */}
-          <div>2:49</div>
+          <div>{(duration && !Number.isNaN(duration)) && calculateTime(duration)}</div>
 
         </div>
       </section>

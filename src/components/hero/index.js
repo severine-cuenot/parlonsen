95a4d385d7283654420b5405aabbb29e77bb3114
Séverine Cuenot <nine.injections@gmail.com>
2024-02-09 10:15:19 +0100

    :art: style: better style for Hero and started to put right imgs

--- src/components/hero/index.js
+++ src/components/hero/index.js
@@ -9,7 +9,7 @@ function Hero() {
       <div className="hero__left-block">
         <header className="hero__title header">Parlons-en&nbsp;! - Grenoble</header>
         <p className="hero__text">Un espace de débat et de projets par les habitant.e.s de la rue et de la ville. </p>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ea inventore sed minima praesentium quia aliquid quaerat quis facere eius, saepe officiis libero, culpa molestias. Excepturi reprehenderit minus fugit cumque laudantium.</p>
+        {/* <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ea inventore sed minima praesentium quia aliquid quaerat quis facere eius, saepe officiis libero, culpa molestias. Excepturi reprehenderit minus fugit cumque laudantium.</p> */}
       </div>
       <div className="hero__right-block">
         <img src={Immeubles} alt="Des immeubles" className="hero__right-block-img" />

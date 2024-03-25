bdbd5c484dade3aa3be8a7d5120ea0a52cc14f20
Séverine Cuenot <nine.injections@gmail.com>
2024-03-25 10:22:50 +0100

    :wrench: config: added 404 page

--- src/pages/404/index.js
+++ src/pages/404/index.js
@@ -1,8 +1,23 @@
+// REACT import
+import { Link } from 'react-router-dom';
+
+// Components import
+import Container from '../../components/Container';
+import Button from '../../components/Button';
+
+import './style.scss';
+
 function NotFound() {
   return (
-    <div>
-      <h1 className="header">Ceci ets la page 404</h1>
-    </div>
+    <Container className="container__notFound">
+      <section className="notFound">
+        <header className="coeur__header header">
+          <h2>Erreur 404&nbsp;!</h2>
+        </header>
+        <p>La page que vous chercher n'existe pas :(</p>
+        <Link to="/"><Button type="button" label="Retourner à l'accueil" btnstyle="coeur" /></Link>
+      </section>
+    </Container>
   );
 }
 
